46a3951d5d7933be0434bdbfdfc1e2e4f37698ad
EdogawaCoder <edogawa.araujo@gmail.com>
2025-10-21 21:44:37 -0300

    Criação do banco de dados,tabelas e stored procedures,Desenvolvimento do repositório e finalização do cadastro do funcionário no controlador

--- ControleDeFuncionarios/Controllers/FuncionarioController.cs
+++ ControleDeFuncionarios/Controllers/FuncionarioController.cs
@@ -1,4 +1,6 @@
 ﻿using ControleDeFuncionarios.Entiles;
+using ControleDeFuncionarios.Enums;
+using ControleDeFuncionarios.Repositories;
 using ControleDeFuncionarios.Services;
 using System;
 using System.Collections.Generic;
@@ -32,10 +34,13 @@ namespace ControleDeFuncionarios.Controllers
                 funcionario.Salario = decimal.Parse(Console.ReadLine() ?? string.Empty);
                 Console.WriteLine("informe o Cep...");
                 var cep= Console.ReadLine() ?? string.Empty;
+
                 //criando um objeto da classe de serviço
                 var viacepService=new ViaCepService();
+
                 //consultando o endereço pelo CEP
                 var endereco = viacepService.ObterEndereco(cep);
+
                 //imprimindo o endereco no consolde (prompt)
                 Console.WriteLine("\nEndereco Obtido:\n");
                 Console.WriteLine(endereco);
@@ -44,10 +49,27 @@ namespace ControleDeFuncionarios.Controllers
                 funcionario.Endereco.Logradouro = json.RootElement.GetProperty("logradouro").GetString() ?? string.Empty;
                 funcionario.Endereco.Bairro = json.RootElement.GetProperty("bairro").GetString() ?? string.Empty;
                 funcionario.Endereco.Localidade = json.RootElement.GetProperty("localidade").GetString() ?? string.Empty;
-                funcionario.Endereco.Uf = json.RootElement.GetProperty("UF").GetString() ?? string.Empty;
-                funcionario.Endereco.Cep = json.RootElement.GetProperty("Cep").GetString() ?? string.Empty;
+                funcionario.Endereco.Uf = json.RootElement.GetProperty("uf").GetString() ?? string.Empty;
+                funcionario.Endereco.Cep = json.RootElement.GetProperty("cep").GetString() ?? string.Empty;
+                
                 Console.WriteLine("\nInforme o numero do endereço:");
                 funcionario.Endereco.Numero = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("\nCargos:");
+                foreach (var item in Enum.GetValues(typeof(Cargo)))
+                {
+                    var valor = (int)item;
+                    var nome = item.ToString();
+
+                    Console.WriteLine($"\t{valor}-{nome}");
+
+                }
+                Console.WriteLine("\n informe o cargo desejado");
+                funcionario.cargo = (Cargo) int.Parse(Console.ReadLine()??string.Empty);
+                var funcionarioRepository = new FuncionarioRepository();
+                funcionarioRepository.Inserir(funcionario);
+
+                Console.WriteLine("Funcionário cadastrado com sucesso!");
             }
             catch(Exception e)
             {
---
+++ ControleDeFuncionarios/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,46 @@
+﻿using ControleDeFuncionarios.Entiles;
+using ControleDeFuncionarios.Enums;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeFuncionarios.Repositories
+{///<sumary>   
+    ///repositorio de banco de dados para a entidade de funcionario.
+   /// </sumary>
+    public class FuncionarioRepository
+    {
+        public void Inserir(Funcionario funcionario)
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDFuncionarios;Integrated Security=True;";
+           
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Execute("Sp_inserir_funcionario", new
+                {
+                    @nome = funcionario.Nome,
+                    @cpf = funcionario.Cpf,
+                    @matricula = funcionario.Matricula,
+                    @DATAADMISSAO = funcionario.DataAdmissao,
+                    @salario = funcionario.Salario,
+                    @Cargo = (int)funcionario.cargo,
+                    @logradouro = funcionario.Endereco.Logradouro,
+                    @numero = funcionario.Endereco.Numero,
+                    @bairro = funcionario.Endereco.Bairro,
+                    @localidade = funcionario.Endereco.Localidade,
+                    @uf = funcionario.Endereco.Uf,
+                    @cep = funcionario.Endereco.Cep,
+                
+                }, 
+                commandType: CommandType.StoredProcedure
+                );
+            }
+
+        }
+    }
+}
